486f8104a7617c7844692ce237e43dd11232c2b6
Suraj Khanal <58101903+surjkhanal@users.noreply.github.com>
2021-10-26 23:07:34 +0530

    card feature

---
+++ script.js
@@ -0,0 +1,125 @@
+
+
+let array = [
+    {
+        "title": "Cotton Tshirts",
+        "price": 123,
+        "description": "Fabric : Cotton Sleeve Length : Short Sleeves Pattern : Solid Multipack : 1 Sizes : S (Chest Size : 38 in, Length Size: 26.5 in) XL (Chest Size : 41 in, Length Size: 28 in) L (Chest Size : 40 in, Length Size: 27.5 in) M (Chest Size : 39 in, Length Size: 27 in) 100% Cotton , Antibacterial , Bio Wash Country of Origin : India",
+
+        "category": "men's clothing",
+        "image": "https://images.meesho.com/images/products/57996471/2y4fk_512.jpg",
+        "rating": {
+            "rate": 4.3,
+            "count": 62
+        }
+    },
+    {
+        "title": "Siyaram Trends off shoulder rayon printed midi dress",
+        "price": 290,
+        "description": "Fabric : Rayon Pattern : Printed Combo of : Single Sizes : XL (Bust Size : 42 in) L (Bust Size : 40 in) M (Bust Size : 38 in) XXL (Bust Size : 44 in) Siyaram Trends brings off shoulder midi dress which is comfortable to wear a day long for party as well as casual wear",
+
+        "category": "women's clothing",
+        "image": "https://images.meesho.com/images/products/57999271/kjpi3_512.jpg",
+        "rating": {
+            "rate": 3.8,
+            "count": 270
+        }
+    },
+    {
+        "title": "Patron Trousers",
+        "price": 634,
+        "description": "Fabric : Viscose Rayon Pattern : Self-Design Multipack : 1 Exclusive formals for men Sizes : 34 (Waist Size : 34 in, Length Size: 43 in)",
+
+        "category": "men's clothing",
+        "image": "https://images.meesho.com/images/products/58011638/qecp4_512.jpg",
+        "rating": {
+            "rate": 3.4,
+            "count": 21
+        }
+    },
+    {
+        "title": "GSP Basic Jet Black High Rise Skinny Fit Woman's Jean",
+        "price": 240,
+        "description": "Fabric : Cotton Blend Multipack : 1 Sizes : 34, 36, 28, 30, 32 Skinny Fit High Rise Jet Black Jeans With Soft & stretchable Fabric. Model Wearing Size 30 Height 5'8",
+
+        "category": "men's clothing",
+        "image": "https://images.meesho.com/images/products/57949960/wtkxd_512.jpg",
+        "rating": {
+            "rate": 3.4,
+            "count": 21
+        }
+    },
+    // {
+    //     "title": "Striped Women Jumpsuit",
+    //     "price": 298,
+    //     "description": "Fabric : Poly Crepe Sleeve Length : Short Sleeves Multipack : 1 Sizes : S (Bust Size : 35 in, Length Size: 52 in, Waist Size: 29 in, Hip Size: 38 in) XL (Bust Size : 41 in, Length Size: 53 in, Waist Size: 36 in, Hip Size: 45 in) XS (Bust Size : 33 in, Length Size: 51 in, Waist Size: 27 in, Hip Size: 36 in) L (Bust Size : 40 in, Length Size: 53 in, Waist Size: 34 in, Hip Size: 43 in) M (Bust Size : 38 in, Length Size: 52 in, Waist Size: 31 in, Hip Size: 40 in) This Jumpsuit has Awesome Look as you can see image. Very Satisfied Jumpsuit Ever! Guaranteed !! We are selling Best Quality Products at affordable price. BUY NOW and try it.",
+
+    //     "category": "men's clothing",
+    //     "image": "https://images.meesho.com/images/products/57968949/esnbo_512.jpg",
+    //     "rating": {
+    //         "rate": 4.3,
+    //         "count": 48
+    //     }
+    // },
+    // {
+    //     "title": "Kurta with chudidaar pajama",
+    //     "price": 325,
+    //     "description": "Top Fabric : Cotton Bottom Fabric : Cotton Scarf Fabric : Cotton Sleeve Length : Long Sleeves Bottom Type : Churidar Pant Stitch Type : Stitched Pattern : Striped Sizes : S (Top Length Size : 42 in, Top Waist Size: 39 in, Bottom Waist Size: 46 in, Bottom Length Size: 43 in) XL (Top Length Size : 44 in, Top Waist Size: 45 in, Bottom Waist Size: 49 in, Bottom Length Size: 43 in) L (Top Length Size : 43 in, Top Waist Size: 42 in, Bottom Waist Size: 48 in, Bottom Length Size: 43 in) XXXL (Top Length Size : 46 in, Top Waist Size: 49 in, Bottom Waist Size: 50 in, Bottom Length Size: 43 in) M (Top Length Size : 43 in, Top Waist Size: 40 in, Bottom Waist Size: 47 in, Bottom Length Size: 43 in) XXL (Top Length Size : 45 in, Top Waist Size: 47 in, Bottom Waist Size: 50 in, Bottom Length Size: 43 in) Tibra Collection as a brand brings you a quality and affordability. We take pride in our commitment to maintain quality standards in each piece manufactured by us . Look your ethnic best wearing this embroidery with zari kurta pajama set for men from the fashion house of Tibra Collection . Made by Dupion silk regular fit kurta pajama set comprises a full-sleeved kurta and churidaar pajama to complete your look . Team this outfit embroidered mojris and rock the party that you attend",
+
+    //     "category": "men's clothing",
+    //     "image": "https://images.meesho.com/images/products/58005704/3v7uf_512.jpg",
+    //     "rating": {
+    //         "rate": 4.1,
+    //         "count": 294
+    //     }
+    // },
+    // {
+    //     "title": "Ethnic Jackets",
+    //     "price": 788,
+    //     "description": "Fabric : Art Silk Sleeve Length : Sleeveless Pattern : Self-Design Combo of : Single Sizes : S (Chest Size : 36 in, Length Size: 28 in, Waist Size: 34 in, Hip Size: 36 in, Shoulder Size: 17 in) XL (Chest Size : 42 in, Length Size: 29 in, Waist Size: 40 in, Hip Size: 42 in, Shoulder Size: 18 in) L (Chest Size : 40 in, Length Size: 29 in, Waist Size: 37 in, Hip Size: 40 in, Shoulder Size: 18 in) M (Chest Size : 38 in, Length Size: 28 in, Waist Size: 36 in, Hip Size: 38 in, Shoulder Size: 17 in) XXL (Chest Size : 44 in, Length Size: 30 in, Waist Size: 42 in, Hip Size: 44 in, Shoulder Size: 18 in) XXXL (Chest Size : 46 in, Length Size: 30 in, Waist Size: 44 in, Hip Size: 46 in, Shoulder Size: 19 in) Ethinic Jacket Country of Origin : India",
+
+    //     "category": "men's clothing",
+    //     "image": "https://images.meesho.com/images/products/56939878/uslfd_512.jpg",
+    //     "rating": {
+    //         "rate": 4.8,
+    //         "count": 24
+    //     }
+    // },
+    // {
+    //     "title": "women casual jumpsuit",
+    //     "price": 128,
+    //     "description": "Fabric : Crepe Sleeve Length : Sleeveless Pattern : Solid Multipack : 1 Sizes : S (Bust Size : 36 in, Length Size: 52 in, Waist Size: 28 in) XL (Bust Size : 42 in, Length Size: 52 in, Waist Size: 34 in) L (Bust Size : 40 in, Length Size: 52 in, Waist Size: 32 in) M (Bust Size : 38 in, Length Size: 52 in, Waist Size: 30 in) women casual jumpsuit,fabric poly crepe with side pockect and gyp closer and elatic waist",
+
+    //     "category": "men's clothing",
+    //     "image": "https://images.meesho.com/images/products/57645010/gd5bp_512.jpg",
+    //     "rating": {
+    //         "rate": 3.4,
+    //         "count": 3624
+    //     }
+    // }
+]
+
+
+
+function createCard(data){
+    let newCard = document.createElement('div');
+    newCard.className ='card'
+    newCard.innerHTML =`
+    <img src="${data.image}" alt="">
+    <div class="card-data">
+    <h2 class="card-title">${data.title}</h2>
+    <div class="card-meta">
+        <span class="card-price">${data.price}</span>
+        <span class="card-rating">${data.rating.rate}
+            <span class="card-rating-count">${data.rating.count}</span>
+        </span>
+    </div>
+    </div>`
+
+    return newCard;
+}
+
+let cardCollection = document.getElementById('sec-cards');
+array.forEach(arr=>{
+    cardCollection.appendChild(createCard(arr));
+})
